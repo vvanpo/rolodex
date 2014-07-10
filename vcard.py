423fc5ff2eb5a88bb133c9e55e73eb45bb3c97fa
Victor van Poppelen <vvanpo@gmail.com>
2014-07-10 16:49:52 -0400

    vcard.py: Took a shortcut in some of the rfc specs, can make it more strict later

--- vcard.py
+++ vcard.py
@@ -8,38 +8,79 @@ import pyparsing as pp
 
 version="4.0"
 
+pp.ParserElement.setDefaultWhitespaceChars("")
+
 class vCard(object):
-	def load(self, stream):
-		# "Core" ABNF from rfc5234
-		CRLF = pp.Literal("\r\n")
-	#	VCHAR = 
-		WSP = pp.Word(" \t", exact=1)
-		NON_ASCII = 
-		vcard = pp.Literal("BEGIN:VCARD") + CRLF + pp.Literal("VERSION:" + \
-				version) + CRLF + pp.OneOrMore(contentline) + \
-				pp.Literal("END:VCARD") + CRLF
-		contentline = pp.Optional(group + pp.Literal(".")) + name + \
-				pp.ZeroOrMore(pp.Literal(";") + param) + pp.Literal(":") + \
-				value + CRLF
-		group = pp.Word(alphanums + "-")
-		name = pp.oneOf("SOURCE KIND FN N NICKNAME PHOTO BDAY ANNIVERSARY \
-				GENDER ADR TEL EMAIL IMPP LANG TZ GEO TITLE ROLE LOGO ORG \
-				MEMBER RELATED CATEGORIES NOTE PRODID REV SOUND UID \
-				CLIENTPIDMAP URL KEY FBURL CALADRURI CALURI XML".split(), \
-				caseless=True) | iana-token | x-name
-		iana-token = group
-		x-name = pp.oneOf(["x-", "X-"]) + group
-		param = language_param | value_param | pref_param | pid_param | \
-				type_param | geo_parameter | tz_parameter | sort_as_param | \
-				calscale_param | any_param
-		param_value = pp.CharsNotIn('"') | pp.QuotedString('"')
-		any_param = (iana-token | x-name) + pp.Literal("=") + param_value + \
-				pp.ZeroOrMore(pp.Literal(",") + param_value)
-		value = 
+	def __init__(self, stream):
+		self.vcard = []
 
+		crlf = pp.Literal("\r\n").suppress()
+		begin = pp.Literal("BEGIN:VCARD")
+		begin.setParseAction(lambda: self.vcard.append([]))
+		ver = pp.Literal("VERSION:" + version)
+		def endAction():
+			for p in self.vcard[-1]:
+				if p["name"].upper() == "FN":
+					return
+			raise Exception("FN property required in vcard")
+		end = pp.Literal("END:VCARD")
+		end.setParseAction(endAction)
+		folded = ~end + pp.Combine(pp.OneOrMore(pp.Word(pp.printables + " \t") \
+				| pp.Suppress(pp.Literal("\r\n ") | pp.Literal("\r\n\t")))) + \
+				crlf
+		vcard = begin + crlf + ver + crlf + pp.OneOrMore(folded) + end + crlf
+		vcard_entity = pp.OneOrMore(vcard)
+#		vcard_entity.setDebug()
 
-		vcard_entity = pp.OneOrMore(vcard).leaveWhitespace()
+		identifier = lambda n: pp.Word(pp.alphanums + "-").setResultsName(n)
+		group = identifier("group")
+		name = identifier("name")
+		param_value = pp.CharsNotIn('":;') | pp.QuotedString('"')
+		param_value.setResultsName("parameter value")
+		param = identifier("parameter") + pp.Literal("=").suppress() + \
+				param_value + pp.ZeroOrMore(pp.Literal(",") + param_value)
+		value = pp.Word(pp.printables + " \t").setResultsName("property value")
+		contentline = pp.Optional(group + pp.Literal(".").suppress()) + name + \
+				pp.ZeroOrMore(pp.Literal(";").suppress() + param) + \
+				pp.Literal(":").suppress() + value
+
+		def propertyAction(tok):
+			v = dict(contentline.parseString(tok[0]))
+			self.vcard[-1].append(v)
+		folded.setParseAction(propertyAction)
+		vcard_entity.parseString(stream)
 	def dump(self):
-		pass
+		print(self.vcard)
 
-
+example = """BEGIN:VCARD\r
+VERSION:4.0\r
+FN:Simon Perreault\r
+N:Perreault;Simon;;;ing. jr,M.Sc.\r
+BDAY:--0203\r
+ANNIVERSARY:20090808T1430-0500\r
+GENDER:M\r
+LANG;PREF=1:fr\r
+LANG;PREF=2:en\r
+ORG;TYPE=work:Viagenie\r
+ADR;TYPE=work:;Suite D2-630;2875 Laurier;\r
+ Quebec;QC;G1V 2M2;Canada\r
+TEL;VALUE=uri;TYPE="work,voice";PREF=1:tel:+1-418-656-9254;ext=102\r
+TEL;VALUE=uri;TYPE="work,cell,voice,video,text":tel:+1-418-262-6501\r
+EMAIL;TYPE=work:simon.perreault@viagenie.ca\r
+GEO;TYPE=work:geo:46.772673,-71.282945\r
+KEY;TYPE=work;VALUE=uri:\r
+ http://www.viagenie.ca/simon.perreault/simon.asc\r
+TZ:-0500\r
+URL;TYPE=home:http://nomis80.org\r
+END:VCARD\r
+BEGIN:VCARD\r
+VERSION:4.0\r
+UID:urn:uuid:4fbe8971-0bc3-424c-9c26-36c3e1eff6b1\r
+FN;PID=1.1:J. Doe\r
+N:Doe;J.;;;\r
+EMAIL;PID=1.1:jdoe@example.com\r
+CLIENTPIDMAP:1;urn:uuid:53e374d9-337e-4727-8803-a1e9c14e0556\r
+END:VCARD\r
+"""
+test = vCard(example)
+test.dump()
